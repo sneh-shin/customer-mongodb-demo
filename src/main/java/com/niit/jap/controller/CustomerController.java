4e208892d95f3259047de90eaaa10298427cf94d
sneh-shin <snehashinde800@gmail.com>
2022-11-24 18:43:14 +0530

    created CustomerController.java in the controller package

---
+++ src/main/java/com/niit/jap/controller/CustomerController.java
@@ -0,0 +1,13 @@
+/*
+ *Author Name: Sneha Shinde
+ *Date: 11/24/2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jap.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class CustomerController {
+}
