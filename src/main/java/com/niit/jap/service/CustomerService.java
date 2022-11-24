076a9f766a4eb8a8079c4d0844e4bc13edb21144
sneh-shin <snehashinde800@gmail.com>
2022-11-24 18:36:12 +0530

    added abstract methods in the CustomerService interface

--- src/main/java/com/niit/jap/service/CustomerService.java
+++ src/main/java/com/niit/jap/service/CustomerService.java
@@ -1,4 +1,12 @@
 package com.niit.jap.service;
 
+import com.niit.jap.domain.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
+    Customer saveCustomer(Customer customer);
+    boolean deleteCustomer(int customerId);
+    List<Customer> getAllCustomers();
+    List<Customer> getByProductName(String productName);
 }
--- src/main/java/com/niit/jap/service/CustomerServiceImpl.java
+++ src/main/java/com/niit/jap/service/CustomerServiceImpl.java
@@ -5,5 +5,30 @@
  */
 package com.niit.jap.service;
 
+import com.niit.jap.domain.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerServiceImpl implements CustomerService{
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteCustomer(int customerId) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getByProductName(String productName) {
+        return null;
+    }
 }
