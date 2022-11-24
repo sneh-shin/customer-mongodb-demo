316e96808d91f79ee9b86c8770bb50bdb30940c5
sneh-shin <snehashinde800@gmail.com>
2022-11-24 18:49:50 +0530

    added @PostMapping to save the customers

--- src/main/java/com/niit/jap/controller/CustomerController.java
+++ src/main/java/com/niit/jap/controller/CustomerController.java
@@ -5,8 +5,13 @@
  */
 package com.niit.jap.controller;
 
+import com.niit.jap.domain.Customer;
 import com.niit.jap.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,9 @@ public class CustomerController {
     @Autowired
     public CustomerController(CustomerServiceImpl service) {
         this.service = service;
+    }
+    @PostMapping("/saveItem")
+    public ResponseEntity<?> saveCustomer(@RequestBody Customer customer){
+        return new ResponseEntity<>(service.saveCustomer(customer), HttpStatus.CREATED);
     }
 }
